12e6a506432ff949b7da00354dd6a99715ceedd9
Nicolas Beeckmans <beeckmans.nicolas@ens.uqam.ca>
2024-02-09 10:36:56 -0500

    Enlever dupliques

--- State.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public abstract class State 
-{
-    protected State state; 
-    protected bool isFinal {get; set;} 
-    public abstract State findNextState(); 
-    public abstract Func<int> getBehavior(); 
-}
